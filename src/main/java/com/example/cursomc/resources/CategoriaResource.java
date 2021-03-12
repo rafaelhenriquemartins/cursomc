087bc76ec1f8b26820ceb061d7d950d0128d73cd
Rafael Martins <rafaelrhm.27@gmail.com>
2021-03-11 21:22:42 -0300

    Criando operacao de instanciacao

--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,6 @@
 package com.example.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,8 +21,6 @@ public class CategoriaResource {
 	public ResponseEntity<?> listaPorId(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-		
-		
 
 	}
 
