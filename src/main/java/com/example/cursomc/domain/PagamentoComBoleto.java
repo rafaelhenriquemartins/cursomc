1fa74c3f08f622c03d205dbc40af625af1d183dd
Rafael Martins <rafaelrhm.27@gmail.com>
2021-03-13 23:45:30 -0300

    Endpoint / pedidos/{id} disponibilizado

--- src/main/java/com/example/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/example/cursomc/domain/PagamentoComBoleto.java
@@ -3,18 +3,17 @@ package com.example.cursomc.domain;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.example.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
-	@Temporal(TemporalType.DATE) 
+	@JsonFormat(pattern="dd/MM/yyyy") 
 	private Date dataVencimento; 
-	@Temporal(TemporalType.DATE) 
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento; 
 	
 	public PagamentoComBoleto() { 
