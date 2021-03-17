a47419cfc1c969803b6da5d39f08a61021ae0ffe
Rafael Martins <rafaelrhm.27@gmail.com>
2021-03-16 23:26:05 -0300

    Vakidacao sintatica com Bean Validation e personalizando o retorno da validacao

--- src/main/java/com/example/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/example/cursomc/dto/CategoriaDTO.java
@@ -1,6 +1,9 @@
 package com.example.cursomc.dto;
 
 import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.example.cursomc.domain.Categoria;
 
@@ -9,6 +12,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatorio")
+	@Length(min=5, max=80, message="O tamanho de ve estar entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
