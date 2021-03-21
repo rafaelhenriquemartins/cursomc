f91a6bf80a101ac43800020be631602a080aef08
Rafael Martins <rafaelrhm.27@gmail.com>
2021-03-21 00:32:44 -0300

    Busca de pedidos por nome e categorias

--- src/main/java/com/example/cursomc/services/PedidoService.java
+++ src/main/java/com/example/cursomc/services/PedidoService.java
@@ -20,4 +20,6 @@ public class PedidoService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException( 
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
 		} 
+	
+	
 }
